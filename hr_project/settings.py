14e07c9813843b58b341f87b19d85eb37844ab81
RohithRr7188 <rohithrr825@example.com>
2025-09-07 22:50:26 +0530

    Prepare settings for Render + Postgres + Whitenoise

--- hr_project/settings.py
+++ hr_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zkkf(!rb%tiq7@2nbzz&kbflx@%9upvs$7w7(7le__r-(o9nuz'
+SECRET_KEY1 = 'django-insecure-zkkf(!rb%tiq7@2nbzz&kbflx@%9upvs$7w7(7le__r-(o9nuz'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+# Hosts - render sets RENDER_EXTERNAL_HOSTNAME — fall back to localhost for dev
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS",
+                os.environ.get("RENDER_EXTERNAL_HOSTNAME","localhost")).split(",")
+
 
 
 # Application definition
@@ -43,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,11 +81,12 @@ WSGI_APPLICATION = 'hr_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
@@ -121,6 +128,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "role_redirect"
